3fa8d0aa176de179b12c15f4a66e5bd8386aa8b2
Cheater121 <106826455+Cheater121@users.noreply.github.com>
2023-06-07 14:38:14 +0300

    Update config.py

--- config_data/config.py
+++ config_data/config.py
@@ -1,37 +1,28 @@
 from dataclasses import dataclass
-
 from environs import Env
 
+
 @dataclass
-
 class TgBot:
-
     token: str
-
     chats: list[int]
 
+
 @dataclass
-
 class TCSClient:
-
     token: str   
 
+
 @dataclass
-
 class Config:
-
     tcs_client: TCSClient
-
     tg_bot: TgBot
 
+
 def load_config(path: str = None) -> Config:
-
     env: Env = Env()
-
     env.read_env(path)
 
     return Config(tg_bot=TgBot(token=env('TG_5MIN_TOKEN'), chats=list(map(int, env.list('CHATS')))),
-
                   tcs_client=TCSClient(token=env('TCS_TOKEN')))
-
                                     
